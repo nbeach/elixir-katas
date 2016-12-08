9f1a3007e369f8e5866a688f1dc90d9f23275e52
Nicholas Beach <nicholascbeach@gmail.com>
2016-12-08 18:15:38 -0500

    Adjusted FizzBuzz test naming

--- test/fizzbuzz_test.exs
+++ test/fizzbuzz_test.exs
@@ -2,22 +2,22 @@ defmodule FizzBuzzTest do
   use ExUnit.Case
   doctest FizzBuzz
 
-  test "evauluate when a number is given returns the number" do
+  test "evauluate() when a number is given returns the number" do
     assert FizzBuzz.evaluate(1) == "1"
     assert FizzBuzz.evaluate(4) == "4"
   end
 
-  test "evaluate when a multiple of 3 is given returns fizz" do
+  test "evaluate() when a multiple of 3 is given returns fizz" do
     assert FizzBuzz.evaluate(3) == "fizz"
     assert FizzBuzz.evaluate(9) == "fizz"
   end
 
-  test "evaluate when a multiple of 5 is given returns buzz" do
+  test "evaluate() when a multiple of 5 is given returns buzz" do
     assert FizzBuzz.evaluate(10) == "buzz"
     assert FizzBuzz.evaluate(25) == "buzz"
   end
 
-  test "evaluate when a multiple of 3 and 5 is given returns fizzbuzz" do
+  test "evaluate() when a multiple of 3 and 5 is given returns fizzbuzz" do
     assert FizzBuzz.evaluate(15) == "fizzbuzz"
     assert FizzBuzz.evaluate(30) == "fizzbuzz"
   end
