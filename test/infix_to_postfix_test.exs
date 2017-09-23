9f563bdde9a4bb9c1e06d4ba2b7557e89885cadc
Nicholas Beach <nicholascbeach@gmail.com>
2017-09-23 16:48:58 -0400

    Added describe blocks to clean up tests

--- test/infix_to_postfix_test.exs
+++ test/infix_to_postfix_test.exs
@@ -1,36 +1,20 @@
 defmodule InfixToPostfixTest do
-  use ExUnit.Case
-  doctest InfixToPostfix
+    use ExUnit.Case
+    doctest InfixToPostfix
 
-  test "convert() converts expressions with only addition" do
-    assert InfixToPostfix.convert("a + b + c") == "ab+c+"
-  end
+    describe "converts an infix expression to postfix when it contains" do
+        test "addition",       do: assert InfixToPostfix.convert("a + b + c") == "ab+c+"
+        test "subtraction",    do: assert InfixToPostfix.convert("a - b - c") == "ab-c-"
+        test "multiplication", do: assert InfixToPostfix.convert("a * b * c") == "ab*c*"
+        test "division",       do: assert InfixToPostfix.convert("a / b / c") == "ab/c/"
+        test "exponents",      do: assert InfixToPostfix.convert("a ^ b ^ c") == "ab^c^"
+        test "parenthesis",    do: assert InfixToPostfix.convert(" a * ( b + c )") == "abc+*"
 
-  test "convert() converts expressions with only subtraction" do
-    assert InfixToPostfix.convert("a - b - c") == "ab-c-"
-  end
+        test "operators of mixed precedence",                 do:
+            assert InfixToPostfix.convert("a ^ b + c - d * e / f") == "ab^c+de*f/-"
 
-  test "convert() converts expressions with only multiplication" do
-    assert InfixToPostfix.convert("a * b * c") == "ab*c*"
-  end
+        test "operators of mixed precedence and parenthesis", do:
+            assert InfixToPostfix.convert("a * ( b + c * d ^ e ) + f") == "abcde^*+*f+"
+    end
 
-  test "convert() converts expressions with only division" do
-    assert InfixToPostfix.convert("a / b / c") == "ab/c/"
-  end
-
-  test "convert() converts expressions with only exponents" do
-    assert InfixToPostfix.convert("a ^ b ^ c") == "ab^c^"
-  end
-
-  test "convert() converts expressions with operators of mixed precedence" do
-    assert InfixToPostfix.convert("a ^ b + c - d * e / f") == "ab^c+de*f/-"
-  end
-
-  test "convert() converts expressions with parenthesis" do
-    assert InfixToPostfix.convert(" a * ( b + c )") == "abc+*"
-  end
-
-  test "convert() converts expressions with operators of mixed precedence and parenthesis" do
-    assert InfixToPostfix.convert("a * ( b + c * d ^ e ) + f") == "abcde^*+*f+"
-  end
 end
