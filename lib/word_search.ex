ea229a4b88cc9e923395432aa6cb2870e1c2e522
Nicholas Beach <nicholas.beach@accenture.com>
2019-06-18 13:53:02 -0400

    Cleanup of word search logic

--- lib/word_search.ex
+++ lib/word_search.ex
@@ -2,35 +2,69 @@ defmodule WordSearch do
   import Enum
 
   def search(word, grid) do
-    all_letter_locations = with_indexes(grid)
+    all_letter_locations = with_coordinates(grid)
     matching_sequences = find_matching_sequences(word, all_letter_locations)
 
     "#{word}: #{format_sequences(matching_sequences)}"
   end
 
   defp format_sequences(sequences) do
-    formatted_sequences = sequences
-      |> map(&format_sequence/1)
-      |> join(" - ")
+    sequences
+    |> map(&format_sequence/1)
+    |> join(" - ")
   end
 
   defp format_sequence(sequence) do
     map(sequence, fn {_, x, y} -> "(#{x},#{y})" end)
-      |> join(", ")
+    |> join(", ")
   end
 
   defp find_matching_sequences(word, all_letter_locations) do
-    [first_letter | other_letters] = String.graphemes(word)
     location_to_letter_map = to_location_to_letter_map(all_letter_locations)
 
-    filter(all_letter_locations, fn {letter, _, _} -> letter === first_letter end)
-      |> map(&(generate_potential_match_sequences(&1, other_letters)))
-      |> filter(&(sequence_exists_in?(&1, location_to_letter_map)))
+    all_letter_locations
+    |> filter(&(is_first_letter_of?(&1, word)))
+    |> flat_map(&(generate_potential_match_sequences_for_starting_location(&1, word)))
+    |> filter(&(sequence_exists_in?(&1, location_to_letter_map)))
   end
 
-  defp generate_potential_match_sequences({letter, x, y}, other_letters) do
-    other_letter_locations = with_index(other_letters) |> map(fn {letter, index} -> {letter, x + index + 1, y } end)
-    [{letter, x, y}] ++ other_letter_locations
+  defp is_first_letter_of?({letter, _, _}, word) do
+    letter === String.at(word, 0)
+  end
+
+  defp generate_potential_match_sequences_for_starting_location(first_letter_location, word) do
+    directional_letter_offset_generators()
+    |> map(fn offset_generator -> create_sequence(first_letter_location, word, offset_generator) end)
+  end
+
+  defp create_sequence(first_letter_location, word, offset_generator) do
+    coordinate_offsets_for_letter = fn (letter, offset) ->
+      {x_offset, y_offset} = offset_generator.(offset)
+      {letter, x_offset, y_offset}
+    end
+
+    {_, initial_x, initial_y} = first_letter_location
+
+    other_letter_locations = word
+    |> String.graphemes()
+    |> tl()
+    |> with_index()
+    |> map(fn {letter, index} -> {letter, index + 1} end)
+    |> map(fn {letter, offset} -> coordinate_offsets_for_letter.(letter, offset) end)
+    |> map(fn {letter, x_offset, y_offset} -> {letter, initial_x + x_offset, initial_y + y_offset} end)
+
+    [first_letter_location] ++ other_letter_locations
+  end
+
+  defp directional_letter_offset_generators() do
+   horizontal = fn offset -> {offset, 0} end
+   vertical = fn offset -> {0, offset} end
+   diagonal_downwards = fn offset -> {offset, offset} end
+   diagonal_upwards = fn offset -> {offset, 0-offset} end
+   generators = [horizontal, vertical, diagonal_downwards, diagonal_upwards]
+
+   reverse_direction = fn generator -> &(generator.(0-&1)) end
+   generators ++ map(generators, &(reverse_direction.(&1)))
   end
 
   defp sequence_exists_in?(sequence, location_to_letter_map) do
@@ -38,10 +72,10 @@ defmodule WordSearch do
   end
 
   defp letter_exists_at_coordinates?({letter, x, y}, location_to_letter_map) do
-    Map.get(location_to_letter_map, {x, y}) !== nil
+    Map.get(location_to_letter_map, {x, y}) === letter
   end
 
-  defp with_indexes(grid) do
+  defp with_coordinates(grid) do
     grid
     |> map(&with_index/1)
     |> with_index()
--- test/word_search_test.exs
+++ test/word_search_test.exs
@@ -1,15 +1,84 @@
 defmodule WordSearchTest do
   use ExUnit.Case
 
-  test "finds words horizontally" do
+  test "finds words forwards horizontally" do
     grid = [
       ["a", "b", "c"],
       ["o", "n", "e"],
       ["a", "b", "c"],
     ]
-    result = WordSearch.search("one", grid)
-#    IO.inspect(result)
-    assert result == "one: (0,1), (1,1), (2,1)"
+
+    assert WordSearch.search("one", grid) == "one: (0,1), (1,1), (2,1)"
+  end
+
+  test "finds words in reverse horizontally" do
+    grid = [
+      ["a", "b", "c"],
+      ["e", "n", "o"],
+      ["a", "b", "c"],
+    ]
+
+    assert WordSearch.search("one", grid) == "one: (2,1), (1,1), (0,1)"
+  end
+
+  test "finds words forward vertically" do
+    grid = [
+      ["a", "o", "c"],
+      ["a", "n", "c"],
+      ["a", "e", "c"],
+    ]
+
+    assert WordSearch.search("one", grid) == "one: (1,0), (1,1), (1,2)"
+  end
+
+  test "finds words in reverse vertically" do
+    grid = [
+      ["a", "e", "c"],
+      ["a", "n", "c"],
+      ["a", "o", "c"],
+    ]
+
+    assert WordSearch.search("one", grid) == "one: (1,2), (1,1), (1,0)"
+  end
+
+  test "finds words diagonally downwards" do
+    grid = [
+      ["o", "b", "c"],
+      ["a", "n", "c"],
+      ["a", "b", "e"],
+    ]
+
+    assert WordSearch.search("one", grid) == "one: (0,0), (1,1), (2,2)"
+  end
+
+  test "finds words in reverse diagonally downwards" do
+    grid = [
+      ["e", "b", "c"],
+      ["a", "n", "c"],
+      ["a", "b", "o"],
+    ]
+
+    assert WordSearch.search("one", grid) == "one: (2,2), (1,1), (0,0)"
+  end
+
+  test "finds words in reversed diagonally upwards" do
+    grid = [
+      ["a", "b", "o"],
+      ["a", "n", "c"],
+      ["e", "b", "c"],
+    ]
+
+    assert WordSearch.search("one", grid) == "one: (2,0), (1,1), (0,2)"
+  end
+
+  test "finds words diagonally upwards" do
+    grid = [
+      ["a", "b", "e"],
+      ["a", "n", "c"],
+      ["o", "b", "c"],
+    ]
+
+    assert WordSearch.search("one", grid) == "one: (0,2), (1,1), (2,0)"
   end
 
 end
