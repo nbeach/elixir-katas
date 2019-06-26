a33e4a72ec6178ae07c139fe66af510bf4b2bcec
Nicholas Beach <nicholas.beach@accenture.com>
2019-06-26 11:14:57 -0400

    Seprated state managment from the vending machine

--- lib/vending_machine/state_manager.ex
+++ lib/vending_machine/state_manager.ex
@@ -1,45 +1,50 @@
 defmodule StateManager do
+
   defmacro __using__(opts) do
     store_name = Keyword.get(opts, :store_name)
     initial_state = Keyword.get(opts, :initial_state)
+    module_to_wrap = Macro.expand(Keyword.get(opts, :module), __ENV__)
 
-    [{name, arity} | _] = VendingMachine.__info__(:functions)
-    |> Keyword.to_list()
-
-#    IO.inspect(functions)
-
-
-    quote do
+    lifecycle_functions = quote do
       def start(), do: Agent.start_link(fn -> unquote(initial_state) end, name: unquote(store_name))
       def stop(), do: Agent.stop(unquote(store_name))
+    end
 
-      def wrap(operation), do: update_state_with(unquote(store_name), fn state -> operation.(state) end)
-      def wrap(operation, arg1), do: update_state_with(unquote(store_name), fn state -> operation.(state, arg1) end)
-      def wrap(operation, arg1, arg2), do: update_state_with(unquote(store_name), fn state -> operation.(state, arg1, arg2) end)
-      def wrap(operation, arg1, arg2, arg3), do: update_state_with(unquote(store_name), fn state -> operation.(state, arg1, arg2, arg3) end)
+    IO.inspect(module_to_wrap)
 
-      defp update_state_with(store_name, operation) do
-        prior_state = get_state(store_name)
-        {result, new_state} = operation.(prior_state)
-        update_state(store_name, new_state)
+    wrapped_functions = module_to_wrap.__info__(:functions)
+    |> Keyword.to_list()
+    |> Enum.filter(fn {_, arity} -> arity !== 0 end)
+    |> Enum.map(&(wrap_function_with_state(module_to_wrap, store_name, &1)))
+
+    [lifecycle_functions] ++ wrapped_functions
+  end
+
+  defp wrap_function_with_state(module, store_name, {name, arity}) do
+    arguments = generate_arguments(arity - 1)
+
+    quote do
+      def unquote(name)(unquote_splicing(arguments)) do
+        get_state = fn store_name -> Agent.get(store_name, &(&1)) end
+        update_state = fn store_name, new_state -> Agent.update(store_name, fn _ -> new_state end) end
+
+        state = get_state.(unquote(store_name))
+        {result, new_state} = apply(unquote(module), unquote(name), [state, unquote_splicing(arguments)])
+        update_state.(unquote(store_name), new_state)
         result
-      end
-
-      defp get_state(store_name) do
-        Agent.get(store_name, &(&1))
-      end
-
-      defp update_state(store_name, new_state) do
-        Agent.update(store_name, fn _ -> new_state end)
-      end
-
-
-      def unquote(name)() do
-        apply(VendingMachine, unquote(name), [get_state(unquote(store_name))])
       end
     end
   end
 
+  defp generate_arguments(arity) do
+    case arity do
+      0 -> []
+      _ -> Enum.map(1..arity, &generate_argument/1)
+    end
+  end
 
+  defp generate_argument(number) do
+    Macro.var(:"arg#{number}", __MODULE__)
+  end
 
 end
--- lib/vending_machine/vending_machine_application.ex
+++ lib/vending_machine/vending_machine_application.ex
@@ -1,7 +1,6 @@
 defmodule VendingMachineApplication do
-  use StateManager, store_name: __MODULE__, initial_state: VendingMachine.new()
-
-#  def display(), do: wrap(&VendingMachine.display/1)
-  def insert_coin(coin), do: wrap(&VendingMachine.insert_coin/2, coin)
-  def empty_coin_return(), do: wrap(&VendingMachine.empty_coin_return/1)
+  use StateManager,
+      module: VendingMachine,
+      store_name: __MODULE__,
+      initial_state: VendingMachine.new()
 end
