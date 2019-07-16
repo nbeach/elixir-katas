fbf50e793d8d5dbb379aa8500c8d86220aa6b0f9
Nicholas Beach <nicholas.beach@accenture.com>
2019-07-16 08:48:13 -0400

    Added genserver implementation of vending machine

---
+++ lib/vending_machine/vending_machine_server.ex
@@ -0,0 +1,69 @@
+defmodule VendingMachineServer do
+  use GenServer
+  import Map, only: [put: 3, update!: 3]
+
+  defstruct coin_return: [], coins: [], credit: [], message: nil, inventory: []
+
+  def init(state) do
+    {:ok, state}
+  end
+
+  def handle_call(:display, _from, state) do
+    cond do
+      state.message !== nil -> {:reply, state.message, put(state, :message, nil)}
+      !Enum.empty?(state.credit) -> {:reply, format_credit(state.credit), state}
+      true -> {:reply, "INSERT COIN", state}
+    end
+  end
+
+  def handle_call({:insert_coin, coin}, _from, state) do
+    coin_value = Coins.get_coin_value(coin)
+    if(coin_value === :invalid) do
+      {:reply, :invalid_coin, update!(state, :coin_return, &([coin] ++ &1))}
+    else
+      {:reply, :ok, update!(state, :credit, &(&1 ++ [coin]))}
+    end
+  end
+
+  def handle_call(:empty_coin_return, _from, state) do
+    {:reply, state.coin_return, put(state, :coin_return, [])}
+  end
+
+  def handle_call(:list_products, _from, state) do
+    {:reply, Inventory.list_products(state.inventory), state}
+  end
+
+  def handle_call({:dispense, product_name}, _from, state) do
+    {item, updated_inventory} = Inventory.dispense_item(state.inventory, product_name)
+
+    cond do
+      item === nil -> {:reply, false, put(state, :message, "SOLD OUT")}
+      item.product.price > Coins.get_credit_value(state.credit) -> {:reply, false, put(state, :message,  format_value(item.product.price)) }
+      true -> {:reply, true, state
+                             |> put(:inventory, updated_inventory)
+                             |> update!(:coin_return, &(&1 ++ Coins.make_change(state.credit, item.product.price)))
+                             |> put(:credit, [])
+                             |> put(:message, "THANK YOU")
+              }
+    end
+  end
+
+  def handle_cast(:return_coins, state) do
+    new_state = state
+    |> update!(:coin_return, &(state.credit ++ &1))
+    |> put(:credit, [])
+
+    {:noreply, new_state}
+  end
+
+  defp format_credit(credit) do
+    credit
+    |> Coins.get_credit_value()
+    |> format_value()
+  end
+
+  defp format_value(value) do
+    Float.to_string(value / 100)
+  end
+
+end
---
+++ test/vending_machine/vending_machine_server_test.exs
@@ -0,0 +1,195 @@
+defmodule VendingMachineServerTest do
+  use ExUnit.Case
+
+  setup do
+    {:ok, pid} = GenServer.start_link(VendingMachineServer, %VendingMachineServer{
+      inventory: [
+        %{ product: %{ name: "Cola", price: 100 }, quantity: 1 },
+        %{ product: %{ name: "Chips", price: 50 }, quantity: 1 },
+        %{ product: %{ name: "Candy", price: 65 }, quantity: 1 }
+      ]
+    })
+    {:ok, pid: pid}
+  end
+
+  describe "when no credit" do
+
+    test "shows an insert coin message on the display", context do
+      assert GenServer.call(context.pid, :display) === "INSERT COIN"
+    end
+
+  end
+
+  describe "when a coin is inserted and it is an accepted coin" do
+
+    test "displays credit for the coin on the display", context do
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      assert GenServer.call(context.pid, :display) === "0.25"
+    end
+
+    test "tells that the coin was accepted", context do
+      assert GenServer.call(context.pid, {:insert_coin, :quarter}) === :ok
+    end
+
+  end
+
+  describe "when a coin is inserted and it is an unrecognized coin" do
+
+    test "tells that the coin was rejected", context do
+      assert GenServer.call(context.pid, {:insert_coin, :sasquatch}) === :invalid_coin
+    end
+
+    test "adds no credit", context do
+      GenServer.call(context.pid, {:insert_coin, :sasquatch})
+      assert GenServer.call(context.pid, :display) == "INSERT COIN"
+    end
+
+    test "puts the coin in the coin return", context do
+      GenServer.call(context.pid, {:insert_coin, :sasquatch})
+      assert GenServer.call(context.pid, :empty_coin_return) == [:sasquatch]
+    end
+
+  end
+
+  describe "when the coin return contents is emptied" do
+
+    test "clears the coin return contents", context do
+      GenServer.call(context.pid, {:insert_coin, :sasquatch})
+      GenServer.call(context.pid, :empty_coin_return)
+      assert GenServer.call(context.pid, :empty_coin_return) == []
+    end
+
+  end
+
+  describe "when the coin return is pushed" do
+
+    test "puts the change in the coin return", context do
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:insert_coin, :nickel})
+
+      GenServer.cast(context.pid, :return_coins)
+
+      assert GenServer.call(context.pid, :empty_coin_return) === [:quarter, :nickel]
+      assert GenServer.call(context.pid, :display) == "INSERT COIN"
+    end
+
+  end
+
+  test "tells what products are available", context do
+    assert GenServer.call(context.pid, :list_products) === [
+         %{ name: "Cola", price: 100 },
+         %{ name: "Chips", price: 50 },
+         %{ name: "Candy", price: 65 }
+       ]
+  end
+
+
+  describe "when a product is selected and it is in stock and there is sufficient credit" do
+
+    test "dispenses the product", context do
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+
+      assert GenServer.call(context.pid, {:dispense, "Chips"}) == true
+    end
+
+    test "displays thank you on the display", context do
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:dispense, "Chips"})
+
+      assert GenServer.call(context.pid, :display) == "THANK YOU"
+    end
+
+    test "displays insert coin after displaying the thank you", context do
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:dispense, "Chips"})
+      GenServer.call(context.pid, :display)
+
+      assert GenServer.call(context.pid, :display) == "INSERT COIN"
+    end
+
+    test "depletes product inventory upon dispensing", context do
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:dispense, "Chips"})
+
+      assert GenServer.call(context.pid, {:dispense, "Chips"}) === false
+      assert GenServer.call(context.pid, :display) == "SOLD OUT"
+    end
+
+    test "returns the remaining change", context do
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:dispense, "Candy"})
+
+      assert GenServer.call(context.pid, :empty_coin_return) === [:dime]
+    end
+
+  end
+
+  describe "when a product is selected and it is in stock and there is insufficient credit" do
+
+    test "does not dispense the product", context do
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      assert GenServer.call(context.pid, {:dispense, "Candy"}) === false
+    end
+
+    test "displays the product price on the display", context do
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:dispense, "Candy"})
+      assert GenServer.call(context.pid, :display) === "0.65"
+    end
+
+    test "returns to displaying the current credit after displaying the product price", context do
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:dispense, "Candy"})
+      GenServer.call(context.pid, :display)
+
+      assert GenServer.call(context.pid, :display) === "0.25"
+    end
+
+  end
+
+  describe "when a product is selected and it is in stock and it is sold out" do
+
+    test "does not dispense the product", context do
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:dispense, "Chips"})
+
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+
+      assert GenServer.call(context.pid, {:dispense, "Chips"}) === false
+    end
+
+    test "displays sold out on the display", context do
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:dispense, "Chips"})
+
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:dispense, "Chips"})
+
+      assert  GenServer.call(context.pid, :display) === "SOLD OUT"
+    end
+
+    test "returns to displaying insert coin after displaying sold out", context do
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:dispense, "Chips"})
+
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:insert_coin, :quarter})
+      GenServer.call(context.pid, {:dispense, "Chips"})
+      GenServer.call(context.pid, :display)
+
+      assert  GenServer.call(context.pid, :display) === "0.5"
+    end
+  end
+
+end
